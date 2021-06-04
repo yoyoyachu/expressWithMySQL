32ece17b210d715e94f156a4094bf52c7632952b
yoyoyachu <ryachna@gmail.com>
2021-06-03 20:12:27 -0400

    insert into sql database using form datain express

--- app.js
+++ app.js
@@ -1,52 +1,83 @@
+const express = require('express');
+const path = require('path');
 
-// var mysql = require('mysql');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+var mysql = require('mysql');
 
 // var con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "example",
-//   database: "class"
+//     host: "localhost",
+//     user: "root",
+//     password: "example",
+//     database: "class"
 // });
 
 
-// con.connect(function(err) {
-//     if (err) throw err;
-//   con.query("SELECT * FROM students WHERE name='ruchi'", function (err, result, fields) {
-//     if (err) throw err;
-//     console.log(result);
-//   });
-// });
-
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-    var mysql = require('mysql');
-
-    var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "example",
-    database: "class"
-    });
-
-
-    con.connect(function(err) {
-        if (err) throw err;
-    con.query("SELECT * FROM students ", function (err, result, fields) {
-        if (err) throw err;
-        const resu = result;
-        console.log(res);
-        for(let student in resu){
-            console.log(student);
-            res.send(student)
-        }
-    });
-    });
+var pool = mysql.createPool({
+    connectionLimit: 5000,
+    host: 'localhost',
+    user: 'root',
+    password: 'example', 
+    database: 'class'
 });
 
-var server = app.listen(5000, function () {
-    console.log('Server is running..');
+
+//home page
+app.get('/home',(req,res)=>{
+    res.send('data inserted')
+})
+
+
+//new form
+app.get('/new',  (req, res)=> {
+    res.render('new')
+});
+
+
+// add data
+app.post('/',async (req,res)=>{
+    const {name,age} = req.body;
+    const sql = "INSERT INTO `student` (`name`,`age`) VALUES ('"+name+"','"+age+"')";
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
+        connection.query(sql, async(er, result, fields)=> {
+            if (er) throw er;
+            console.log(result);
+        });
+    });
+    res.redirect('/');
+});
+
+//read
+app.get('/', async(req, res) =>{
+    pool.getConnection(function(err, connection) {
+        if (err) throw err;
+        connection.query("SELECT * FROM student ", async(er, result, fields)=> {
+            if (er) throw er;
+            console.log(result);
+            res.render('view',{result})
+        });
+    });
+    // con.connect(async(err) =>{
+    //     if (err) throw err;
+    //     con.query("SELECT * FROM students ", async(er, result, fields)=> {
+    //         if (er) throw er;
+    //         console.log(result);
+    //         res.render('view',{result})
+    //     });
+    // });
+    
+});
+
+
+
+app.listen(5000, () => {
+    console.log(`Listening on port 5000`)
 });
 
 
